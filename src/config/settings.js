4e49fea0057aeb96226e5b60f167ebaf93b82506
Andrew Ford <andrew@andrewford.co.nz>
2024-08-15 11:30:47 +1200

    Added more options for Sandpack settings

--- src/config/settings.js
+++ src/config/settings.js
@@ -8,15 +8,17 @@ const sandPackOptions = {
     autoReload: true,
     showInlineErrors: true, // default - false
     wrapContent: true, // default - false
-    // editorHeight: "100%", // default - 300
-    // previewHeight: "100%", // default - 500
+    layout: "preview", // "preview" | "tests" | "console";
     editorHeight: "calc(100%)", // default - 300
     editorHeightPercentage: 50, // default - 50
     editorPreviewHeight: "calc(100%)", // default - 500
     editorWidthPercentage: 70, // default - 50
-    showConsole: false, // default - false
     resizablePanels: true, // default - false
-
+    // previewHeight: "100%", // default - 500
+    // showConsole: true, // default - false
+    // editorHeight: "100%", // default - 300
+    // showPreview: true,
+    // showConsoleButton: true,
 };
 
 
