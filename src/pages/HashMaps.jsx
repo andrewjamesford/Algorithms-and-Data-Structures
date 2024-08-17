6d2f7d9c74956d7998d6f32392bba9bc5750932d
Andrew Ford <andrew@andrewford.co.nz>
2024-08-17 22:30:43 +1200

    Added biomejs formatting updates

--- src/pages/HashMaps.jsx
+++ src/pages/HashMaps.jsx
@@ -1,36 +1,34 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
 
-import Example from "../examples/Graphs/Example.js?raw";
-import index from "../examples/Graphs/index.js?raw";
-import { sandPackOptions } from "../config/settings";
+import Example from "../examples/HashMaps/Example.js?raw";
+import index from "../examples/HashMaps/index.js?raw";
+import { sandPackOptions, sandPackProps } from "../config/settings";
 
 function HashMaps() {
-  return (
-    <div>
-      <h1 className="text-4xl">Hash Maps / Tables Page</h1>
-      <p className="py-4">
-        A hash map is a data structure that maps keys to values using a hash
-        function. In JavaScript, the Map object is a built-in implementation of
-        a hash map that allows any value (including objects and functions) to be
-        used as a key or a value.
-      </p>
+	return (
+		<div>
+			<h1 className="text-4xl">Hash Maps / Tables Page</h1>
+			<p className="py-4">
+				A hash map is a data structure that maps keys to values using a hash
+				function. In JavaScript, the Map object is a built-in implementation of
+				a hash map that allows any value (including objects and functions) to be
+				used as a key or a value.
+			</p>
 
-      <Sandpack
-        template="node"
-        theme="dark"
-        files={{
-          "Example.js": Example,
-          "index.js": index,
-        }}
-        customSetup={{
-          entry: index,
-        }}
-        autoRun={true}
-        options={sandPackOptions}
-        showNavigation={true}
-      />
-    </div>
-  );
+			<Sandpack
+				files={{
+					"Example.js": Example,
+					"index.js": index,
+				}}
+				customSetup={{ entry: index }}
+				options={sandPackOptions}
+				template={sandPackProps.template}
+				theme={sandPackProps.theme}
+				autoRun={sandPackProps.autoRun}
+				showNavigation={sandPackProps.showNavigation}
+			/>
+		</div>
+	);
 }
 
 export default HashMaps;
